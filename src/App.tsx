bc9e1f280c9d0636284ed070d608cfa3f26343df
Alessandra Amicarella <alessamica@gmail.com>
2023-04-18 23:43:06 -0400

    Mobile header component and remove no used styles from the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0);
+import "./App.css";
 
+function App() {
   return (
     <div className="App">
       <Header />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,18 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import logo from "../assets/logo.svg";
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="border flex justify-between items-center w-full bg-blue-1 p-5">
+      <img src={logo} alt="Macropay logo" className="w-28" />
+      <div className="border text-xs flex items-center justify-evenly w-3/4">
+        <button className="text-blue-2 bg-yellow- h-7 w-24 rounded-sm">
+          Crea tu cuenta
+        </button>
+        <button className="text-white">Iniciar sesión</button>
+        <div className="bg-gray-100 w-5 h-5 rounded-full flex justify-center items-center">
+          <AiOutlineShoppingCart color="gray" />
+        </div>
+      </div>
+    </header>
+  );
 };
