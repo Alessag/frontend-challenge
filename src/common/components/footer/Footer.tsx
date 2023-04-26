e5fc16d097ad233d533cae9cec4cffc49e79562b
Alessandra Amicarella <alessamica@gmail.com>
2023-04-26 13:20:55 -0400

    Footer structure

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,3 +1,26 @@
 export const Footer = () => {
-  return <div className="h-96 bg-green-300">Footer</div>;
+  return (
+    <div className="h-96 bg-green-300">
+      <p>Ofertas y promociones</p>
+      <h1>No te pierdas nuestras ofertas!</h1>
+      <input type="text" placeholder="Tu direccion de correo electronico " />
+      <p>logo</p>
+      <p>Todos los derechos reservados - c Macroplay 2022</p>
+      <ul>
+        <li>envios y devoluciones</li>
+        <li>terminos y condiciones</li>
+        <li>Preguntas Frecuentes</li>
+        <p>Aviso de privacidad</p>
+      </ul>
+      <p>Smile face </p>
+      <p>Conversemos!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur</p>
+      <ul>
+        <li>facebook</li>
+        <li>instagram</li>
+        <li>linkedin</li>
+        <li>whatsapp</li>
+      </ul>
+    </div>
+  );
 };
