3d364bd6fb34e3f5390fb2529a7f5b1a2dce4b13
Alessandra Amicarella <alessamica@gmail.com>
2023-04-27 22:20:12 -0400

    Mobile search bar styles

--- src/common/components/searchbar/SearchBar.tsx
+++ src/common/components/searchbar/SearchBar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { TextField } from "@mui/material";
 import { FiSearch } from "react-icons/fi";
 import useViewport from "../../utils/hooks/useViewport";
+import { AiFillCaretDown } from "react-icons/ai";
 
 interface SearchBarProps {
   onSearch: (searchValue: string) => void;
@@ -18,20 +19,21 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch, label }) => {
   };
 
   return (
-    <div className="border-2 border-black flex text-xs box-border my-5">
-      <div className="flex items-center rounded-l-2xl bg-white p-2 lg:p-4 h-11">
+    <div className="border-2 border-black flex text-sm box-border my-5">
+      <div className="flex items-center rounded-l-2xl bg-white p-3 lg:p-4 h-11">
         <FiSearch color="#7D879C" />
       </div>
       <input
         placeholder={viewport === "sm" || viewport === "md" ? "Buscar" : label}
         value={searchValue}
         onChange={handleSearch}
-        className="h-11 text-gray-5"
+        className="h-11 text-gray-5 w-44 outline-none"
       />
-      <div className="border-2 bg-gray-1 flex items-center rounded-r-2xl lg:w-48 text-center text-gray-4 h-11">
+      <div className="border-2 bg-gray-1 flex justify-center items-center rounded-r-2xl w-28 lg:w-48 text-center text-gray-4 h-11">
         {viewport === "sm" || viewport === "md"
           ? "Categorías"
           : "Todas las categorías"}
+        <AiFillCaretDown className="ml-2" />
       </div>
     </div>
   );
