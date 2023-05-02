bb2f5cea7d2bcc44f3e0806df24a1cd3f735102f
Alessandra Amicarella <alessamica@gmail.com>
2023-05-01 22:36:56 -0400

    Product list responsive styles

--- src/modules/home/ProductList.tsx
+++ src/modules/home/ProductList.tsx
@@ -35,18 +35,18 @@ export const ProductList: React.FC<ProductListProps> = ({
   handleSearchChange,
 }) => {
   return (
-    <div className="flex flex-col 2xl:justify-center lg:flex-row max-w-7xl 2xl:max-w-full mx-auto px-5 xl:px-0">
+    <div className="flex flex-col 2xl:justify-start lg:flex-row max-w-7xl 3xl:max-w-[1640px] mx-auto px-5">
       <Filters
         handleFilterChange={handleFilterChange}
         selectedFilters={selectedFilters}
         category={parseCategory(location.pathname.substring(1))}
       />
-      <div className="flex flex-col justify-start items-start">
+      <div className="flex flex-col justify-start items-start w-full">
         <SearchBar
           onSearch={(searchValue) => handleSearchChange(searchValue)}
           label="Encuentra el producto que necesitas"
         />
-        <div className="grid justify-items-center  grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 md:gap-16 lg:gap-[51px] w-full h-[600px] lg:h-[800px] overflow-y-scroll">
+        <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 3xl:grid-cols-3 xl:gap-[51px] w-full h-[600px] lg:h-[800px] overflow-y-scroll">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((product) => {
               return (
